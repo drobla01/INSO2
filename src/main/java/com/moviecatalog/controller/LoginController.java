f7938c0eaf7a785aea7b5335a956107c2d72da65
Mario <msancm16@estudiantes.unileon.es>
2018-06-02 21:16:36 +0200

    login styles

--- src/main/java/com/moviecatalog/controller/LoginController.java
+++ src/main/java/com/moviecatalog/controller/LoginController.java
@@ -7,8 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.moviecatalog.model.Results;
 import com.moviecatalog.model.User;
 import com.moviecatalog.service.UserService;
 
