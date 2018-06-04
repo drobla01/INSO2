25feea302d705f5b1931cca33c2f31aec647797c
David <drobla01@estudiantes.unileon.es>
2018-06-04 22:03:04 +0200

    Redirect fix

--- src/main/java/com/moviecatalog/controller/SocialController.java
+++ src/main/java/com/moviecatalog/controller/SocialController.java
@@ -9,24 +9,27 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
 import com.moviecatalog.model.User;
 import com.moviecatalog.service.UserService;
 
 @Controller
 public class SocialController {
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@ModelAttribute
 	public void addAttributes(Model model) {
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		User user = userService.findUserByEmail(auth.getName());
-		if(user != null)
+		if (user != null)
 			model.addAttribute("sesion", user);
 	}
-	
-	@RequestMapping(value="/user/social", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/user/social", method = RequestMethod.GET)
 	public String goToProfile(@RequestParam(name = "id", required = true) Integer id, Model model) {
 		User user = userService.findUserById(id);
 		model.addAttribute("user", user);
@@ -36,10 +39,14 @@ public class SocialController {
 		model.addAttribute("follows", user.getFollows());
 		return "user/social";
 	}
-	
-	@RequestMapping(value="/user/social", method = RequestMethod.POST)
-	public String addFriend(@RequestParam(name = "id", required = true) Integer id, Model model) {
-		return "/user/social"; //return "forward:/user/social?id="+id; esta genera el bucle infinito
+
+	@RequestMapping(value = "/user/social", method = RequestMethod.POST)
+	public ModelAndView addFriend(@RequestParam(name = "id", required = true) Integer id, Model model) {
+		ModelAndView mav = new ModelAndView(new RedirectView("/user/social", true));
+		mav.addObject("id", id);
+		return mav;
+		// return "/user/social"; //return "forward:/user/social?id="+id; esta
+		// genera el bucle infinito
 	}
-	
+
 }
