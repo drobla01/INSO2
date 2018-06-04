d051fbf8c4a6118bb36e00aca0d94c52add479f8
Alejandro Campos <acampc01@estudiantes.unileon.es>
2018-06-04 21:40:42 +0200

    Social Tabs and add friend post

--- src/main/java/com/moviecatalog/controller/SocialController.java
+++ src/main/java/com/moviecatalog/controller/SocialController.java
@@ -1,19 +1,14 @@
 package com.moviecatalog.controller;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-
 import com.moviecatalog.model.User;
 import com.moviecatalog.service.UserService;
 
@@ -43,10 +38,8 @@ public class SocialController {
 	}
 	
 	@RequestMapping(value="/user/social", method = RequestMethod.POST)
-	public  ModelAndView addFriend() {
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("user/social");
-		return modelAndView;
+	public String addFriend(@RequestParam(name = "id", required = true) Integer id, Model model) {
+		return "/user/social"; //return "forward:/user/social?id="+id; esta genera el bucle infinito
 	}
 	
 }
